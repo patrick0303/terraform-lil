c818cb83748dfd40ffb0151be55c97c7a09b3544
Patrick Lok <plok@ch-nb-2647.uptake.com>
2018-09-28 15:17:50 -0500

    add comments to cidr block creation for subnet

--- resources.tf
+++ resources.tf
@@ -6,3 +6,42 @@ resource "aws_vpc" "environment-example-two" {
     Name = "terraform-aws-vpc-example-two"
   }
 }
+
+resource "aws_subnet" "subnet1" {
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block,3 ,1)}"
+  availability_zone = "us-east-1a"
+}
+# Outcome 10.0.32.0/19
+# cidrsubnet(iprange, newbits, netnum)
+# required bitmask = original bitmask /16 + newbits 3 = /19
+
+# Original: 10.0.0.0/16
+# Expected: 10.0.32.0/19, it would be (32) ÷ (2 power (24 - 19)) = (32) ÷ (32) = 1
+
+resource "aws_subnet" "subnet2" {
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block,2 ,2 )}"
+  availability_zone = "us-east-1b"
+}
+# Outcome 10.0.128.0/18
+# cidrsubnet(iprange, newbits, netnum)
+# required bitmask = original bitmask /16 + newbits 2 = /18
+
+# Original: 10.0.0.0/16
+# Expected: 10.0.128.0/18, it would be (128) ÷ (2 power (24 - 18)) = (128) ÷ (64) = 2
+
+
+resource "aws_security_group" "subnetsecurity" {
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+
+  ingress {
+    cidr_blocks = [
+    "${aws_vpc.environment-example-two.cidr_block}"
+    ]
+
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+  }
+}
